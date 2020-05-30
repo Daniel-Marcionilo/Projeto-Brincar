55b82b42c81490de9aebbd50cd0ab1924c8de649
Daniel Marcionilo Pedroso do Rosario Silva <danielmarcionilo2002@gmail.com>
2020-05-30 12:40:03 -0300

    Update bd.sql

--- BANCO/bd.sql
+++ BANCO/bd.sql
@@ -10,13 +10,6 @@ descricao varchar(500) not null
 
 
 
-select * from brincadeiras;
-
-select * from brincadeiras where nome ='teste1' and descricao='teste1';
-
-update brincadeiras set nome = '2345', descricao = '980' where codBrinc = 1;
-
-update brincadeiras set nome = 'teste' where codBrinc = 1;
 
 
 
